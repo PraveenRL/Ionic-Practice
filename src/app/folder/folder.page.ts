5cf02617c97f1458f9bc1f9027b750958592662a
PraveenRL <rlppraveen@gmail.com>
2020-05-18 08:54:54 +0530

    Color Change in variable.scss

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -15,4 +15,22 @@ export class FolderPage implements OnInit {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  toggleTheme(event) {
+    let systemDark = window.matchMedia("(prefers-color-scheme: dark)");
+    systemDark.addListener(this.colorTest);
+    if (event.detail.checked) {
+      document.body.setAttribute('data-theme', 'dark');
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }
+
+  private colorTest(systemInitiatedDark) {
+    if (systemInitiatedDark.matches) {
+      document.body.setAttribute('data-theme', 'dark');
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }
+
 }
